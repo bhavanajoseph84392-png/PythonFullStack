457b9d6a77e5ef4793668248c2328a606c782d98
bhavsjosephine8792 <bhavanajoseph84392@gmail.com>
2026-05-13 15:43:39 +0530

    Add files via upload
    On day  I have learned the basic introduction to Python, tokens, identifiers, variables, and types of comments.

---
+++ day1.py
@@ -0,0 +1,69 @@
+'''print("This is my first python class")
+
+''"What is python"?
+'''High level language
+'''----------------------------
+'''--->No need to worry about low level mainment
+
+'''-Dynamically typed language
+'''--------------------------
+'''-->No need to mention the kind of data taking to variable
+
+'''-Interpreter Language
+'''--------------------------------------------
+'''---->Python will execute the code line by line and can through the error if any but all before.
+'''--- when error is identified the program stops execution
+
+
+
+
+
+
+
+
+
+
+
+
+'''Tokens
+'''----------
+'''--->Keywords in the program
+'''if
+'else
+'''while
+'''for
+
+'''Identifiers
+'''-----------------
+'''-->names of variables,functions and classes
+'''
+'''variables :which are used to store data
+'''good ways'''A-Z,a-z,_
+''''''''''
+'bad ways
+'special character,space , numbers,0-9
+'''''''''''
+'''a=90
+'''A=9
+'''s_=89
+
+'create three different variables:
+
+
+
+
+
+
+'''comments
+'''1.Single line comment (#)
+'''2. multi-line ('''''')
+#types of variables:
+1.int a=4
+2.*a= 2
+3. c9=100
+
+
+
+
+
+
