c9cd152dbee84589e69a64158539adc59244f1f3
bhavsjosephine8792 <bhavanajoseph84392@gmail.com>
2026-06-16 10:34:19 +0530

    Add files via upload

---
+++ day27 Time Date.py
@@ -0,0 +1,187 @@
+'''
+Data Analysis:-
+-->This the process of inspecting, cleaning,transforming, and modeling data to discover useful insights...
+Types of DA:
+------------
+1.Descriptive Analysis: Summarizing data
+2.Diagonistic Analysis:Understanding causes
+
+3.Predictive Analysis
+-->Forecasting future connections
+4.Prescriptive Analysis
+--->Suggesting actions based on data
+Why DA:
+------
+--->To improve decision making
+--->Detects Trends and Patterns
+
+Numpys and Pandas(Numerical Python)
+----------------------------------
+-->This python library for numerical computing .it provides support for multi-dimensionsal arrays,and linear algebra operations,making it essential for data analysis
+
+using numpy in DA
+---------------------
+--->Improved performance
+--->Simplifies complex operations
+--->Easy for data manipulation......
+
+import numpy as np
+arr_1 = np.array([[4,5,6,7],[1,2,3,4],[5,6,7,8,]])
+print(arr_1)
+print(arr_1.shape)
+
+import numpy as np
+arr_1 = np.array([[4,5,6,7],[1,2,3,4],[5,6,7,8,]])
+print(arr_1)
+
+
+import numpy as np
+arr_1 = np.array([[4,5,6,7],[1,2,3,4],[5,6,7,8,]])
+print(arr_1)
+print(arr_1.shape)
+reshaped = arr_1.reshape(3,2)
+print(reshaped)
+
+import numpy as np
+arr_1= np.array([1,2],[3,4]])
+arr_2=np.array([[5,6],[7,8]])
+print(np.dot)
+
+
+import numpy as np
+arr_1 = np.array([10,20,30])
+nrm_copy = arr_1.view()
+arr_1[0]=100
+print(nrm_copy)
+print(arr_1)
+
+
+copy_dee = arr_1.copy()
+arr_1[1]= 200
+print(copy_dee)
+print(arr_1)
+
+
+Pandas:-
+-----------
+-->The pandas is powerful data manipulation and analysis library...
+-->where it provides data structure like series and dataframe for efficient data handling.........
+
+
+'''
+import pandas as pd
+any_ = pd.Series([2999,15999,52999,4999,1999]),
+index = ['Earbuds','Smartphone','Laptop','Watch','Footware',]
+
+print(any_)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
